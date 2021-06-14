ea2e7f469e39486dcf9a7a68d651c38ae9ababfa
Chris Camel <camel.christophe@gmail.com>
2021-06-14 07:54:11 +0200

    feat(checkout): configure subscription checkout
    Signed-off-by: Chris Camel <camel.christophe@gmail.com>

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,6 +1,5 @@
 const stripe = Stripe('pk_test_51IBHitCNjnBMsnzg7iz0r6uhkYs2SDj3cyw0wRs3miYWW5DrRORxJnZtC2L8myDxQjI5Gvk3QUkKS4eSo2ttrZAx002eMycirh');
-const enterpriseBasicPriceId = 'price_1IEw9QCNjnBMsnzgKbjfAEUL';
-const enterpriseAdvancedPriceId = 'price_1IIyEVCNjnBMsnzgVOrQKOg9';
+const enterpriseBasicPriceId = 'price_1It9vmCNjnBMsnzgZ2d4NHhN';
 
 function redirectToCheckout(priceId, successUrl, cancelUrl, errorUrl) {
     stripe.redirectToCheckout({
@@ -8,7 +7,7 @@ function redirectToCheckout(priceId, successUrl, cancelUrl, errorUrl) {
           price: priceId,
           quantity: 1,
         }],
-        mode: 'payment',
+        mode: 'subscription',
         successUrl: successUrl,
         cancelUrl: cancelUrl,
       }).then(function (result) {
