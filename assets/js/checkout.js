f75de3e0d2ed5530dfb918e227732b6d9f353334
Chris Camel <camel.christophe@gmail.com>
2021-06-17 17:56:32 +0200

    feat(checkout): go to production mode
    Signed-off-by: Chris Camel <camel.christophe@gmail.com>

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,5 +1,5 @@
-const stripe = Stripe('pk_test_51IBHitCNjnBMsnzg7iz0r6uhkYs2SDj3cyw0wRs3miYWW5DrRORxJnZtC2L8myDxQjI5Gvk3QUkKS4eSo2ttrZAx002eMycirh');
-const enterpriseBasicPriceId = 'price_1It9vmCNjnBMsnzgZ2d4NHhN';
+const stripe = Stripe('pk_live_51IBHitCNjnBMsnzgRCsfiK4wgd8JwZGLqMnPxVAbh880ruHGRziyCyZ4AS7NoInVSXLPNBZBju3hibq7OMcrA2lg00kP6Y6qi7');
+const enterpriseBasicPriceId = 'price_1J3JztCNjnBMsnzgHyUQwSRT';
 
 function redirectToCheckout(priceId, successUrl, cancelUrl, errorUrl) {
     stripe.redirectToCheckout({
